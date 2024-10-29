c1cfd014cba24bf28026dbf7f97e2572e73a6a04
Bryan Khor <bryankhor07@gmail.com>
2024-10-29 13:26:43 -0700

    Added Weather App to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import TenziesPhoto from "../images/TenziesApp.png";
+import WeatherAppPhoto from "../images/WeatherAppPhoto.png";
 
 export default function Projects() {
   return (
@@ -10,9 +11,9 @@ export default function Projects() {
             href="https://tenziesgameforkids123.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
-            className="tenzies--container"
+            className="project--container"
           >
-            <img src={TenziesPhoto} alt="Tenzies" className="tenzies--photo" />
+            <img src={TenziesPhoto} alt="Tenzies" className="project--photo" />
             <div className="overlay--text">
               A fun dice-rolling game where the goal is to match all the dice to
               the same number. Built with React for an interactive and
@@ -22,11 +23,24 @@ export default function Projects() {
           <h3>Tenzies: Game for Kids</h3>
         </div>
         <div className="projects--list">
-          <h3>Project 2</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            pulvinar, nisl ac tincidunt tincidunt, sapi
-          </p>
+          <a
+            href="https://easy-to-use-weatherapp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project--container"
+          >
+            <img
+              src={WeatherAppPhoto}
+              alt="Weather App"
+              className="project--photo"
+            />
+            <div className="overlay--text">
+              A weather app that displays the current weather, temperature, and
+              humidity for a specific city. Created with React and styled with
+              CSS for a clean and modern design.
+            </div>
+          </a>
+          <h3>Weather App: A site for everyone</h3>
         </div>
         <div className="projects--list">
           <h3>Project 3</h3>
